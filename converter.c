43d0c9d3a79cd43676471cf40f2cf769ba6eabe5
Mads <mk@magnutron.dk>
2024-08-29 11:35:10 +0200

    Yes

---
+++ converter.c
@@ -0,0 +1,21 @@
+main()
+{
+    printf("          |\n");
+    printf("        --+--\n");
+    printf("    /  \\  |\n");
+    printf("   |    | |\n");
+    printf("   \\____/ |\n");
+    printf("   ( .. ) |\n");
+    printf("   /\\__/\\ |      WOLOLOOOO\n");
+    printf("  /\\ qp /\\|\n");
+    printf(" /  |  |  |\n");
+    printf("/|  |db| /\\`|\n");
+    printf("| \\ |  | \\|_|\n");
+    printf("\\  \\|qp|  |\n");
+    printf(" \\__/  |  |\n");
+    printf(" |/||db|  |\n");
+    printf(" |  |  |  |\n");
+    printf("-=Welcome to the CONVERTER!=-\n");
+
+    // Run start dialog/menu here.
+}
