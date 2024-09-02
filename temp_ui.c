e2b32647e5c5dc38f042f29ed37b67b100e42f5e
Mads <mk@magnutron.dk>
2024-09-02 20:38:30 +0200

    yes

--- temp_ui.c
+++ temp_ui.c
@@ -162,11 +162,56 @@ void display_distance_menu(){
 void display_weight_menu(){
     printf("Please select an option from the menu below\n");
     printf("1. Convert from Kilograms to Pounds\n");
-    printf("2. Convert from Pounds to Kilograms\n");
-    printf("3. Convert from Kilograms to Ounces\n");
-    printf("4. Convert from Ounces to Kilograms\n");
-    printf("5. Convert from Pounds to Ounces\n");
+    printf("2. Convert from Kilograms to Ounces\n");
+    printf("3. Convert from Pounds to Kilograms\n");
+    printf("4. Convert from Pounds to Ounces\n");
+    printf("5. Convert from Ounces to Kilograms\n");
     printf("6. Convert from Ounces to Pounds\n");
     printf("7. Return to main menu\n");
     printf("_________________________________________________________\n");
+
+    int option;
+    scanf("%d", &option);
+
+    switch (option)
+    {
+    case 1:
+        printf("Enter the weight in Kilograms\n");
+        scanf("%d", &option);
+        printf("The weight in Pounds is %d\n", kilograms_to_pounds(option));
+        break;
+
+    case 2:
+        printf("Enter the weight in Kilograms\n");
+        scanf("%d", &option);
+        printf("The weight in Ounces is %d\n", kilograms_to_ounces(option));
+        break;
+
+    case 3:
+        printf("Enter the weight in Pounds\n");
+        scanf("%d", &option);
+        printf("The weight in Kilograms is %d\n", pounds_to_kilograms(option));
+        break;
+
+    case 4:
+        printf("Enter the weight in Pounds\n");
+        scanf("%d", &option);
+        printf("The weight in Ounces is %d\n", pounds_to_ounces(option));
+        break; 
+
+    case 5:
+        printf("Enter the weight in Ounces\n");
+        scanf("%d", &option);
+        printf("The weight in Kilograms is %d\n", ounces_to_kilograms(option));
+        break;
+
+    case 6:
+        printf("Enter the weight in Ounces\n");
+        scanf("%d", &option);
+        printf("The weight in Pounds is %d\n", ounces_to_pounds(option));
+        break;
+
+    default:
+        break;
+    }
 }
---
+++ weight_converters.c
@@ -0,0 +1,23 @@
+int kilograms_to_ounces(int kilograms) {
+    return kilograms * 35.274;
+}
+
+int kilograms_to_pounds(int kilograms) {
+    return kilograms * 2.20462;
+}
+
+int ounces_to_kilograms(int ounces) {
+    return ounces / 35.274;
+}
+
+int ounces_to_pounds(int ounces) {
+    return ounces / 16;
+}
+
+int pounds_to_kilograms(int pounds) {
+    return pounds * 0.453592;
+}
+
+int pounds_to_ounces(int pounds) {
+    return pounds * 16;
+}
